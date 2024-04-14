9d2edf4eb08db713f92b61e1f95423106028b711
sibims <sibima00@gmail.com>
2024-04-14 11:59:43 +0530

    handled the part url.when user is not entered valid url

--- app.py
+++ app.py
@@ -18,18 +18,35 @@ with st.sidebar:
     st.header("Settings")
     web_url=st.text_input("Website URL")
 
-#Created the chat input
-user_input=st.chat_input("Ask your questions...")
-if user_input is not None and user_input!="":
-    response=get_response(user_input)
-    st.session_state.chat_history.append(HumanMessage(content=user_input))
-    st.session_state.chat_history.append(HumanMessage(content=response))
-    # with st.chat_message("Human"):
-    #     st.write(user_input)
-    # with st.chat_message("AI"):
-    #     st.write(response)
-with st.sidebar:
-    st.write(st.session_state.chat_history)
+#Handling error when user trying to enter the url
+if web_url is None or web_url=='':
+    st.info("Please enter a valid URL")
+else:
+    #Created the chat input
+    user_input=st.chat_input("Ask your questions...")
+    if user_input is not None and user_input!="":
+        response=get_response(user_input)
+        st.session_state.chat_history.append(HumanMessage(content=user_input))
+        st.session_state.chat_history.append(AIMessage(content=response))
+        # with st.chat_message("Human"):
+        #     st.write(user_input)
+        # with st.chat_message("AI"):
+        #     st.write(response)
+    # with st.sidebar:
+    #     st.write(st.session_state.chat_history)
     
+    
+    #Chat conversations
+    for message in st.session_state.chat_history:
+        if isinstance(message,AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message,HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
+        
+
+
+        
 
 
