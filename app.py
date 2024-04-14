c658b1e7e8252582af322b34a66ab1682bdc1c9d
sibims <sibima00@gmail.com>
2024-04-14 11:12:38 +0530

    Created a chat_history

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
 
+def get_response(user_input):
+    return "I dont know"
+
+#App config
 st.set_page_config(page_title="ChatWeb",page_icon='🤖')
 st.title("ChatWeB")
+
+chat_history=[
+    AIMessage(content="Hello, I am ChatWeb.How can i help you?"),
+    
+]
 
 #Created the side bar
 with st.sidebar:
@@ -9,10 +19,15 @@ with st.sidebar:
     web_url=st.text_input("Website URL")
 
 #Created the chat input
-st.chat_input("Ask your questions...")
+user_input=st.chat_input("Ask your questions...")
+if user_input is not None and user_input!="":
+    response=get_response(user_input)
+    chat_history.append(HumanMessage(content=user_input))
+    chat_history.append(HumanMessage(content=response))
+    with st.chat_message("Human"):
+        st.write(user_input)
+    with st.chat_message("AI"):
+        st.write(response)
+    
 
-#Dummy chats
-with st.chat_message("AI"):
-    st.write("How can i help you?")
-with st.chat_message("Human"):
-    st.write("Nothing")
+
