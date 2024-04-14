3b4a04b6edc4b9c1e92034c6b011b3f9e9c0324b
sibims <sibima00@gmail.com>
2024-04-14 11:36:52 +0530

    chat_history variable is added with the session state in langchain for prevent the reinitialize

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ def get_response(user_input):
 st.set_page_config(page_title="ChatWeb",page_icon='🤖')
 st.title("ChatWeB")
 
-chat_history=[
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history=[
     AIMessage(content="Hello, I am ChatWeb.How can i help you?"),
     
 ]
-
 #Created the side bar
 with st.sidebar:
     st.header("Settings")
@@ -22,12 +22,14 @@ with st.sidebar:
 user_input=st.chat_input("Ask your questions...")
 if user_input is not None and user_input!="":
     response=get_response(user_input)
-    chat_history.append(HumanMessage(content=user_input))
-    chat_history.append(HumanMessage(content=response))
-    with st.chat_message("Human"):
-        st.write(user_input)
-    with st.chat_message("AI"):
-        st.write(response)
+    st.session_state.chat_history.append(HumanMessage(content=user_input))
+    st.session_state.chat_history.append(HumanMessage(content=response))
+    # with st.chat_message("Human"):
+    #     st.write(user_input)
+    # with st.chat_message("AI"):
+    #     st.write(response)
+with st.sidebar:
+    st.write(st.session_state.chat_history)
     
 
 
