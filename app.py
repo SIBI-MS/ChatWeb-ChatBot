70d21a66f3722efe1eab0a673792b84c8e1c23e8
sibims <sibima00@gmail.com>
2024-04-14 18:34:20 +0530

    added the retriever

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from langchain_community.vectorstores import Chroma #to store vectors created my
 from langchain.embeddings import FlagEmbeddings
 from dotenv import load_dotenv
 import os
+from langchain_community.llms import HuggingFaceHub
+from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever
+
 
 #for loading the .env file to take api
 load_dotenv()
@@ -39,8 +43,22 @@ def get_chunks(documents):
     documents_chunks=text_splitter.split_documents(documents)
     return documents_chunks
 
-
-
+#creating the model instance
+def get_context_retriever_chain(vector_store):
+    llm=HuggingFaceHub(
+        huggingfacehub_api_token=os.environ['HUGGINGFACE_API_KEY'],
+        repo_id=model_id,
+        model_kwargs={"temperature": 0.7, "max_new_tokens": 500}
+    )
+    retriever=vector_store.as_retriever()
+    prompt=ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ('user',"{input}"),
+        ("user","Nothing")
+    ])
+    retriever_chain=create_history_aware_retriever(llm,retriever,prompt)
+    return retriever_chain
+    
 
 #App config
 st.set_page_config(page_title="ChatWeb",page_icon='🤖')
@@ -60,7 +78,8 @@ with st.sidebar:
 if web_url is None or web_url=='':
     st.info("Please enter a valid URL")
 else:
-    documents_chunks=get_vector_store_from_url(web_url)
+    vector_store=get_vector_store_from_url(web_url)
+    retriever_chain=get_context_retriever_chain(vector_store)
     # with st.sidebar:
     #     st.write(documents_chunks)
     
@@ -70,6 +89,14 @@ else:
         response=get_response(user_input)
         st.session_state.chat_history.append(HumanMessage(content=user_input))
         st.session_state.chat_history.append(AIMessage(content=response))
+        
+        retrieved_documents=retriever_chain.invoke({
+            "chat_history":st.session_state.chat_history,
+            "input":user_input
+            
+        })
+        st.write(retrieved_documents)
+        
         # with st.chat_message("Human"):
         #     st.write(user_input)
         # with st.chat_message("AI"):
