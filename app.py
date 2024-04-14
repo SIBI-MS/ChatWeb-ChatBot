21766fff7c0fc4c3c9a5ce28b46af9db80720f3a
sibims <sibima00@gmail.com>
2024-04-14 15:12:57 +0530

    created a function make chunks

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_community.document_loaders import WebBaseLoader  
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 #for get response to the user
 def get_response(user_input):
     return "I dont know"
 
-#storing the vectors of chunks
-def vector_store_from_url(url):
+#storing the contents from the url
+def get_vector_store_from_url(url):
     loader=WebBaseLoader(url)
     documents=loader.load()
+    documents=get_chunks(documents)
     return documents
+
+#creating chunks using the website contents
+def get_chunks(documents):
+    text_splitter=RecursiveCharacterTextSplitter()
+    documents_chunks=text_splitter.split_documents(documents)
+    return documents_chunks
 
 #App config
 st.set_page_config(page_title="ChatWeb",page_icon='🤖')
@@ -30,6 +38,10 @@ with st.sidebar:
 if web_url is None or web_url=='':
     st.info("Please enter a valid URL")
 else:
+    documents_chunks=get_vector_store_from_url(web_url)
+    with st.sidebar:
+        st.write(documents_chunks)
+    
     #Created the chat input
     user_input=st.chat_input("Ask your questions...")
     if user_input is not None and user_input!="":
