51645169d606ea57f9d712b3dae6ea1d83f50361
sibims <sibima00@gmail.com>
2024-04-16 15:09:35 +0530

    complted.but still have some errors

--- app.py
+++ app.py
@@ -13,126 +13,108 @@ from langchain.chains import create_history_aware_retriever, create_retrieval_ch
 from sentence_transformers import SentenceTransformer
 
 
-#for loading the .env file to take api
-load_dotenv()
-
 #Storing apikey and model name
 os.environ['HUGGINGFACE_API_KEY']=os.getenv("HUGGINGFACE_API_KEY")
 model_id = 'tiiuae/falcon-7b-instruct'
+load_dotenv()
 
-#for get response to the user
-def get_response(user_input):
-    return "I dont know"
-
-#storing the contents from the url
-def get_vector_store_from_url(url):
-    loader=WebBaseLoader(url)
+def get_vectorstore_from_url(url):
+    # get the text in document form
+    loader = WebBaseLoader(url)
+    document = loader.load()
     
-    #store contents from url
-    documents=loader.load()
+    # split the document into chunks
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(document)
     
-    #creating chunks
-    documents_chunks=get_chunks(documents)
-    
-    #storing vectors in chroma
+    # create a vectorstore from the chunks
     embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-    vector_store=Chroma.from_documents(documents_chunks,embeddings)
-    
+    vector_store = Chroma.from_documents(document_chunks,embeddings)
+
     return vector_store
 
-#creating chunks using the website contents
-def get_chunks(documents):
-    text_splitter=RecursiveCharacterTextSplitter()
-    documents_chunks=text_splitter.split_documents(documents)
-    return documents_chunks
-
-#creating the model instance
 def get_context_retriever_chain(vector_store):
-    llm=HuggingFaceHub(
+    llm = HuggingFaceHub(
         huggingfacehub_api_token=os.environ['HUGGINGFACE_API_KEY'],
         repo_id=model_id,
         model_kwargs={"temperature": 0.7, "max_new_tokens": 500}
     )
-    retriever=vector_store.as_retriever()
-    prompt=ChatPromptTemplate.from_messages([
-        MessagesPlaceholder(variable_name="chat_history"),
-        ('user',"{input}"),
-        ("user","Given the above conversation, generate a search query to look up in order to get information relevent to the conversation_")
+    
+    retriever = vector_store.as_retriever()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+      ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
     ])
-    retriever_chain=create_history_aware_retriever(llm,retriever,prompt)
+    
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+    
     return retriever_chain
     
-
-#
-def get_conversational_rag_chain(retriever_chain):
-    llm=HuggingFaceHub(
+def get_conversational_rag_chain(retriever_chain): 
+    llm = HuggingFaceHub(
         huggingfacehub_api_token=os.environ['HUGGINGFACE_API_KEY'],
         repo_id=model_id,
         model_kwargs={"temperature": 0.7, "max_new_tokens": 500}
     )
-    prompt=ChatPromptTemplate.from_messages([
-        ('system','Answer the users questions based on the below context:\n\n{context}'),
-        MessagesPlaceholder(variable_name="chat_history"),
-        ("user","{input}"),
-    ])
-    stuff_documents_chain=create_stuff_documents_chain(llm,prompt)
-
-#App config
-st.set_page_config(page_title="ChatWeb",page_icon='🤖')
-st.title("ChatWeB")
-
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history=[
-    AIMessage(content="Hello, I am ChatWeb.How can i help you?"),
     
-]
-#Created the side bar
+    prompt = ChatPromptTemplate.from_messages([
+      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+    ])
+    
+    stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
+    
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+def get_response(user_input):
+    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+    
+    response = conversation_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_input
+    })
+    
+    return response['answer']
+
+# app config
+st.set_page_config(page_title="Chat with websites", page_icon="🤖")
+st.title("Chat with websites")
+
+# sidebar
 with st.sidebar:
     st.header("Settings")
-    web_url=st.text_input("Website URL")
+    website_url = st.text_input("Website URL")
 
-#Handling error when user trying to enter the url
-if web_url is None or web_url=='':
-    st.info("Please enter a valid URL")
+if website_url is None or website_url == "":
+    st.info("Please enter a website URL")
+
 else:
-    vector_store=get_vector_store_from_url(web_url)
-    retriever_chain=get_context_retriever_chain(vector_store)
-    # with st.sidebar:
-    #     st.write(documents_chunks)
-    
-    #Created the chat input
-    user_input=st.chat_input("Ask your questions...")
-    if user_input is not None and user_input!="":
-        response=get_response(user_input)
-        st.session_state.chat_history.append(HumanMessage(content=user_input))
+    # session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hello, I am a bot. How can I help you?"),
+        ]
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorstore_from_url(website_url)    
+
+    # user input
+    user_query = st.chat_input("Type your message here...")
+    if user_query is not None and user_query != "":
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
         st.session_state.chat_history.append(AIMessage(content=response))
         
-        retrieved_documents=retriever_chain.invoke({
-            "chat_history":st.session_state.chat_history,
-            "input":user_input
-            
-        })
-        st.write(retrieved_documents)
-        
-        # with st.chat_message("Human"):
-        #     st.write(user_input)
-        # with st.chat_message("AI"):
-        #     st.write(response)
-    # with st.sidebar:
-    #     st.write(st.session_state.chat_history)
-    
-    
-    #Chat conversations
+       
+
+    # conversation
     for message in st.session_state.chat_history:
-        if isinstance(message,AIMessage):
+        if isinstance(message, AIMessage):
             with st.chat_message("AI"):
                 st.write(message.content)
-        elif isinstance(message,HumanMessage):
+        elif isinstance(message, HumanMessage):
             with st.chat_message("Human"):
                 st.write(message.content)
-        
-
-
-        
-
-
